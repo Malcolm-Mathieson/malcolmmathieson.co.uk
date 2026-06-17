d4b79b46440513da34a6b645030751ba77a39e90
Malcolm-Mathieson <malcolm.mathieson@icloud.com>
2026-06-17 01:51:28 +0100

    Updated wishlist
    removed received items

--- wishlist.php
+++ wishlist.php
@@ -14,13 +14,6 @@
         <!-- <li>A comfortable (gaming) chair</li><br> not until I have my own accomodation-->
 
         <li>
-          Cyberpunk 2077
-          <ul>
-            <li><a href="https://store.steampowered.com/app/1091500/Cyberpunk_2077/">Steam</a></li>
-          </ul>
-        </li><br>
-
-        <li>
             Death Stranding
             <ul>
                 <li><a href="https://apps.apple.com/gb/app/death-stranding-directors-cut/id6449748961">Mac App Store</a></li>
@@ -32,33 +25,6 @@
           <ul>
             <li><a href="https://apps.apple.com/gb/app/assassins-creed-shadows/id6497794841?mt=12">Mac App Store</a></li>
           </ul>
-        </li><br>
-
-        <li>
-          Dead Island 2
-          <ul>
-            <li><a href="https://store.steampowered.com/app/934700/Dead_Island_2/">Steam</a></li>
-          </ul>
-        </li><br>
-        
-        <li>
-          A fountain pen with a very thin nib, for small writing (smaller than my current one)
-        </li><br>
-
-        <li>
-          Dungeons & Dragons dice set
-        </li><br>
-        
-         <li>
-          A Bambu Lab 3D printer 
-        </li><br>
-        
-        <li>
-          3D printing filament
-        </li><br>
-        
-        <li>
-          An amber necklace, similar so the one I used to have as a child (the light coloured amber, the dark amber is Raine's)
         </li><br>
         
         <li>
